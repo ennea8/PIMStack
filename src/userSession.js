3d860be25527b1300c55207ff7508c5809c2abe1
ennea8 <ennea8@gmail.com>
2020-04-06 23:32:23 +0800

    import radiks

--- src/userSession.js
+++ src/userSession.js
@@ -1,5 +1,13 @@
 import { UserSession, AppConfig } from 'blockstack'
 
+// import { configure, User, getConfig } from 'radiks'
+import { configure } from 'radiks'
+
 export const appConfig = new AppConfig(['store_write', 'publish_data'])
 export const userSession = new UserSession({ appConfig })
 window.userSession = userSession
+
+configure({
+  apiServer: 'http://localhost:1260',
+  userSession
+})
